41e12594ec68908dcc0f11b58f0a3d3ef3fe1e04
Bill McKinney <bmckinney@ebsco.com>
2017-02-10 20:52:03 -0500

    adds simplecov test coverage

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,12 @@
-require 'minitest/autorun'
-require 'ebsco'
 require 'dotenv'
 require 'climate_control'
+require 'simplecov'
 
 Dotenv.load('.env.test')
+SimpleCov.start do
+  add_filter 'test'
+  command_name 'Mintest'
+end
+
+require 'minitest/autorun'
+require 'ebsco'
