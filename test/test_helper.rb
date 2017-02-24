ad84f33fe0c8e9e2890b47956fab1ab41fb376a7
Bill McKinney <bmckinney@ebsco.com>
2017-02-24 15:39:15 -0500

    enables codecov.io updates

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,14 +3,16 @@ require 'climate_control'
 require 'simplecov'
 
 Dotenv.load('.env.test')
+
 SimpleCov.start do
   add_filter 'test'
   command_name 'Mintest'
 end
 
-# require 'codecov'
-# SimpleCov.formatter = SimpleCov::Formatter::Codecov
-# CODECOV_TOKEN='cfc86a29-02eb-43f3-9903-7fd46f9a510d'
+if ENV['CODECOV_TOKEN']
+  require 'codecov'
+  SimpleCov.formatter = SimpleCov::Formatter::Codecov
+end
 
 require 'minitest/autorun'
 require 'ebsco/eds'
